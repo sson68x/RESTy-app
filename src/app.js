f261764022c32966d41299a07314715168c2b709
Seungwoo Son <sson68x@gmail.com>
2022-07-19 15:03:10 -0700

    Lab 26 completed

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './app.scss';
+import Header from './components/header';
+import Footer from './components/footer';
+import Form from './components/form';
+import Results from './components/results';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      requestParams: {},
+    };
+  }
+
+  callApi = (requestParams) => {
+
+    const data = {
+      count: 2,
+      results: [
+        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+      ],
+    };
+    this.setState({data, requestParams});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div>Request Method: {this.state.requestParams.method}</div>
+        <div>URL: {this.state.requestParams.url}</div>
+        <Form handleApiCall={this.callApi} />
+        <Results data={this.state.data} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
